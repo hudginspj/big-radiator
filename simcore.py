5554deacc407d5ad25eb7ca6d5790b58ec95bfe9
hudginspj <hudginspj@mymail.vcu.edu>
2018-09-22 15:52:36 -0400

    two rings

--- simcore.py
+++ simcore.py
@@ -9,7 +9,7 @@ from math import *
 #!!!!!!!!!! Point 1 is acting on point 2
 G = 6.673e-11
 n_points_in_ring = 1000
-ring_mass = 1e24
+
 
 def point_accel(r_1, theta_1, r_2, m):
     x_1 = r_1 * cos(theta_1)
@@ -30,7 +30,7 @@ def point_accel(r_1, theta_1, r_2, m):
 def ring_accel(r_ring_1, r_2):
     total_accel = 0.0
     for i in range(n_points_in_ring//2):
-        theta = 2 * pi * i / n_points_in_ring
+        theta = 2 * pi * (i + 0.5) / n_points_in_ring
         point_mass = ring_mass / n_points_in_ring
         accel = point_accel(r_ring_1, theta, r_2, point_mass)
         #print(theta, accel)
@@ -42,24 +42,81 @@ def ring_accel(r_ring_1, r_2):
 #    print(i, ring_accel(10000, i))
 
 
+# mass_flux & step time -> ring mass
+starting_radius = 1e10
+step_time = 1e5
+ring_mass = 1e30
+class Sim:
+    def __init__(self, mass_flux, initial_velocity):
+        self.mass_flux = mass_flux
+        self.initial_velocity = initial_velocity
+        self.radii = []
+        self.velocities = []
+        self.n_rings = 0
+        self.t = 0
+        #self.add_ring()
+        
+    def add_ring(self):
+        self.radii.append(starting_radius)
+        self.velocities.append(self.initial_velocity)
+        self.n_rings += 1
+        
+        
+    def step(self):
+        self.t += step_time
+        
+        if self.mass_flux * self.t > ring_mass * self.n_rings:
+            
+            #print(self.mass_flux * self.t, ring_mass * self.n_rings)
+            self.add_ring()
+        
+        new_radii = []
+        new_velocities = []
+        accels = []
+        for i in range(self.n_rings): # accelerating ring
+            a = 0.0
+            for j in range(self.n_rings):  #force rings
+                a += ring_accel(self.radii[j], self.radii[i])
+            accels.append(a)
+            v = self.velocities[i]
+            r = self.radii[i]
+            new_radii.append(r + v * step_time + 0.5 * a * step_time * step_time)
+            new_velocities.append(v + a * step_time)
+        #print(self.radii, new_radii)
+        print("radii", new_radii, 'velocities', new_velocities)#, accels)
+        self.radii = new_radii
+        self.velocities = new_velocities
+        
+
+
+sim = Sim(1e24, 1e5)
+for i in range(20):
+    sim.step()
+    
+    
+
+
 
 
 time = 0.0
 v = 1e5
-r = 1e5
-step_time = 1e5
+r = 1e10
 def step():
     global time, v, r
-    time += 1.0
-    a = accel_ring(r, r)
+    time += step_time
+    a = ring_accel(r, r)
     v_new = v + a * step_time
     r_new = r + v * step_time + 0.5 * a * step_time * step_time
     v = v_new
     r = r_new
+    print(v, r, a)
+    return r,v
     
+#while r > 0:
+#    step()
+
 
 if __name__ == "__main__":
     pass
-    #step()
-    #print(step())
+
     
