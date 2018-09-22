219fcb63352b83fbf81c139919741d5676d79ba1
Papa Beye <32583389+PapaBeye@users.noreply.github.com>
2018-09-22 15:12:36 -0400

    file for 3d g

--- 3dVisuals.py
+++ 3dVisuals.py
@@ -1,28 +1,56 @@
 from mpl_toolkits.mplot3d import axes3d
 import matplotlib.pyplot as plt
 from matplotlib import style
-
-
+import dummystep as dt
+import time
+import matplotlib.animation as ani
 style.use('ggplot')
-
 
 fig = plt.figure()
 ax1 = fig.add_subplot(111, projection='3d')
+def animate(a):
+    xs = []
+    ys = []
+    zs = []
+    data = dt.step()
+    for i in data:
+        values = data[i]
+        x = values['x']
+        y = values['y']
+        z = values['z']
+        xs.append(x)
+        ys.append(y)
+        zs.append(z)
+        ax1.set_xlabel('x axis')
+        ax1.set_ylabel('y axis')
+        ax1.set_zlabel('z axis')
+    ax1.scatter(x, y, z, c='g', marker='o')
+    print(xs[0])
+    #ax1.clear()
+   # ax1.scatter(xs, ys, zs, c='g', marker='o')
 
-x = [1,2,3,4,5,6,7,8,9,10]
-y = [5,6,7,8,2,5,6,3,7,2]
-z = [1,2,6,3,2,7,3,3,7,2]
-
-x2 = [-1,-2,-3,-4,-5,-6,-7,-8,-9,-10]
-y2 = [-5,-6,-7,-8,-2,-5,-6,-3,-7,-2]
-z2 = [1,2,6,3,2,7,3,3,7,2]
-
-ax1.scatter(x, y, z, c='g', marker='o')
-ax1.scatter(x2, y2, z2, c ='r', marker='o')
-
+gr = ani.FuncAnimation(fig, animate, interval=1000)
 ax1.set_xlabel('x axis')
 ax1.set_ylabel('y axis')
 ax1.set_zlabel('z axis')
-
 plt.show()
 
+'''
+
+while 1:
+    data = dt.step()
+    for i in data:
+        values = data[i]
+    x = values['x']
+    y = values['y']
+    z = values['z']
+    print(x)
+    print(y)
+    print(z)
+    ax1.scatter(x, y, z, c='g', marker='o')
+    ax1.set_xlabel('x axis')
+    ax1.set_ylabel('y axis')
+    ax1.set_zlabel('z axis')
+    plt.show()
+    time.sleep(1)
+'''
