319ae366571ebe5fc7af9fcd5e4ebdbddf26cb14
hudginspj <hudginspj@mymail.vcu.edu>
2018-09-22 18:46:18 -0400

    balls 3 - good template for multiple balls

--- balls3.py
+++ balls3.py
@@ -15,49 +15,23 @@ import math
 xlim = (0,30)
 ylim = (0,20)
 
-# 1 millisecond delta t
-delta_t = 0.001
-
 fig = plt.figure()
 ax = fig.add_subplot(111, autoscale_on=False, xlim=xlim, ylim=ylim)
 ax.grid()
 
-# in Python 2.7 we have to derive from object to have new-style classes
-# in Python 3 this is still valid, but not necessary, as all classes are new-style
-class Ball(object):
 
-    def __init__(self, y):
-        """
-        :param xy: Initial position.
-        :param v: Initial velocity.
-        """
-        self.y = y
 
-        self.scatter, = ax.plot([], [], 'o', markersize=3)
-
-    def update(self, x):
-        #self.scatter.set_data(self.xy)
-        self.scatter.set_data([x, self.y])
-
-#balls = [Ball(3), Ball(7), Ball(11)]
-
-scatters = [ax.plot([], [], 'o', markersize=3) for i in range(3)]
-
-def init():
-    return []
+scatters = [ax.plot([], [], 'o', markersize=3)[0] for i in range(6)]
 
 def animate(t):
-    # t is time in seconds
-    #for ball in balls:
-    #    ball.update(5*math.sin(t) + 10)
-    for scatter in scatters:
-        scatter.set_data([])
-    # have to return an iterable
-    #return [ball.scatter for ball in balls]
+    # t is count of iterations
+    for i, scatter in enumerate(scatters):
+        x = 5*i
+        y = 5*math.sin(t) + 10
+        scatter.set_data([x, y])
     return scatters
 
-# interval in milliseconds
-# we're watching in slow motion (delta t is shorter than interval)
-ani = animation.FuncAnimation(fig, animate, np.arange(0,100,delta_t), init_func=init, interval=10, blit=True)
+#interval in milliseconds
+ani = animation.FuncAnimation(fig, animate, interval=1000, blit=True)
 
 plt.show()
